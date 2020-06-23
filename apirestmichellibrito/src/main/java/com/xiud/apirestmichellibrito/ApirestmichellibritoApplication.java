99178e63ba735b44290f8261d0da2e800b52c63f
Gabriel-Aragao <gabriellamparina@gmail.com>
2020-06-23 15:53:22 -0300

    initial commit

---
+++ apirestmichellibrito/src/main/java/com/xiud/apirestmichellibrito/ApirestmichellibritoApplication.java
@@ -0,0 +1,13 @@
+package com.xiud.apirestmichellibrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestmichellibritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestmichellibritoApplication.class, args);
+	}
+
+}
